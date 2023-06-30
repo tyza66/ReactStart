be9ffd8a10be1a41cd1f5da7d640268357d3f932
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-30 13:00:07 +0800

    基础完结

--- c6_响应事件和更新界面/make-unit/src/App.js
+++ c6_响应事件和更新界面/make-unit/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function MyGiao() {
   return (
@@ -34,7 +34,38 @@ function MyButton(){
   )
 }
 
+//这个按钮用来演示更新界面的功能
+//这样定义的组件里面的变量的值都是独立的 不会影响到别的组件
+//因为每个组件都拥有自己的state
+function MyButton2(){
+  //一次显示按钮时，count 的值为 0，因为你把 0 传给了 useState()。当你想改变 state 时，调用 setCount() 并将新的值传递给它。点击该按钮计数器将递增
+  const [count,setCount] = useState(0);
+
+  function add(){
+    setCount(count+1);
+  }
+  return(
+    <button onClick={add}>
+      点我的话{count}会变
+    </button>
+  );
+}
+
+//这个将是用来演示同时更新效果用的Button
+function MyButton3({ count, onClick }){
+  return (
+    <button onClick={onClick}>点我改变一起的按钮{count}</button>
+  );
+}
+
 function App() {
+
+  const [count,setCount] = useState(0);
+
+  function add(){
+    setCount(count+1);
+  }
+
   return (
     <div className="App">
       这下面是另一个我自定义的组件giao
@@ -43,6 +74,12 @@ function App() {
       <br/>
       {giao3}
       <MyButton />
+      <MyButton2 />
+      <MyButton2 />
+      <MyButton3 count={count} onClick={add}/>
+      <MyButton3 count={count} onClick={add}/>
+      <br/>
+
     </div>
   );
 }
